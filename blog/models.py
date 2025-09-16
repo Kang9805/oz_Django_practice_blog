ad7122197ba5352e66242bd5b86b910e63da4d12
kang9805 <zzionkk@naver.com>
2025-09-16 16:30:10 +0900

    Django 3일차

---
+++ blog/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+
+class Blog(models.Model):
+    CATEGORY_CHOICES = (
+        ('free', '자유'),
+        ('travel', '여행'),
+        ('football', '축구'),
+        ('cat', '고양이'),
+    )
+
+    category = models.CharField('카테고리', max_length=30, choices=CATEGORY_CHOICES)
+    title = models.CharField(max_length=100)
+    content = models.TextField('본문')
+
+    created_at = models.DateTimeField('작성일자',auto_now_add=True)
+    updated_at = models.DateTimeField('수정일자', auto_now=True)
+
+    def __str__(self):
+        return f'[{self.get_category_display()}] {self.title[:10]}'
+
+    class Meta:
+        verbose_name = '블로그'
+        verbose_name_plural = '블로그 목록'
+
+
+
+# 제목
+# 본문
+# 작성일자
+# 수정일자
+# 카테고리
+# 작성자 => 패스(추후 업데이트)
+
+
