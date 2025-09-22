c160010da03fba45e20f3efcc50666f22181fa54
kang9805 <zzionkk@naver.com>
2025-09-22 15:09:15 +0900

    Django 7일차

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,12 @@
+from io import BytesIO
+
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
 
 from utils.models import TimestampModel
+from PIL import Image
+from pathlib import Path
 
 User = get_user_model()
 
@@ -19,6 +23,13 @@ class Blog(TimestampModel):
     title = models.CharField(max_length=100)
     content = models.TextField('본문')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField('이미지', null=True, blank=True, upload_to='blog/%Y/%m/%d')
+    thumbnail = models.ImageField('썸네일', null=True, blank=True, upload_to='blog/%Y/%m/%d/thumbnail')
+    # 2025/9/22일
+    # blog/2025/9/22/이미지파일.jpg
+    # ImageFields는  FileField와 같지만 이미지만 업로드하게 되어있다.
+    # varchar => 실제로는 경로만 저장을 함
+
     # models.CASCADE -> 같이 삭제
     # models.PROTECT -> 삭제가 불가능함 (유저를 삭제하려고할때 블로그가 있으면 유저 삭제 불가능)
     # models.SET_NULL -> 널 값을 넣는다 ( 유저 삭제시 블로그의 author가 null이 됨)
@@ -28,6 +39,45 @@ class Blog(TimestampModel):
 
     def get_absolute_url(self):
         return reverse('blog:detail', kwargs={'blog_pk': self.pk})
+
+    def get_thumbnail_image_url (self):
+        if self.thumbnail:
+            return self.thumbnail.url
+        elif self.image:
+            return self.image.url
+        return None
+
+
+    def save(self, *args, **kwargs):
+        if not self.image:
+            return super().save(*args, **kwargs)
+
+        image = Image.open(self.image)
+        image.thumbnail((300, 300))
+
+        image_path = Path(self.image.name)
+
+        thumbnail_name = image_path.stem # /blog/2025/09/22/database.png => database
+        thumbnail_extension = image_path.suffix.lower() # /blog/2025/09/22/database.png => .png
+        thumbnail_filename = f'{thumbnail_name}_thumb{thumbnail_extension}' # database_thumb.png
+
+        if thumbnail_extension in ['.jpg', '.jpeg']:
+            file_type = 'JPEG'
+        elif thumbnail_extension == '.gif':
+            file_type = 'GIF'
+        elif thumbnail_extension == '.png':
+            file_type = 'PNG'
+        else:
+            return super().save(*args, **kwargs)
+
+        temp_thumb = BytesIO()
+        image.save(temp_thumb, file_type)
+        temp_thumb.seek(0)
+
+        self.thumbnail.save(thumbnail_filename, temp_thumb, save=False)
+        temp_thumb.close()
+        return super().save(*args, **kwargs)
+
 
     class Meta:
         verbose_name = '블로그'
--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,7 @@ OWN_APPS = [
 
 THIRD_PARTY_APPS = [
     'django_extensions',
+    'django_summernote',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + OWN_APPS + THIRD_PARTY_APPS
@@ -132,6 +133,11 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = BASE_DIR / '.static_root'
+
+# media
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
@@ -139,8 +145,84 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # login
 LOGIN_REDIRECT_URL = '/'
-
 LOGIN_URL = '/login/'
-
-# logout
 LOGOUT_REDIRECT_URL = '/'
+
+# summernote
+SUMMERNOTE_CONFIG = {
+    # Or, you can set it to `False` to use SummernoteInplaceWidget by default - no iframe mode
+    # In this case, you have to load Bootstrap/jQuery sources and dependencies manually.
+    # Use this when you're already using Bootstrap/jQuery based themes.
+    'iframe': True,
+
+    # You can put custom Summernote settings
+    'summernote': {
+        # As an example, using Summernote Air-mode
+        'airMode': False,
+
+        # Change editor size
+        'width': '100%',
+        'height': '480',
+
+        # Use proper language setting automatically (default)
+
+        # Toolbar customization
+        # https://summernote.org/deep-dive/#custom-toolbar-popover
+        'toolbar': [
+            ['style', ['style']],
+            ['font', ['bold', 'underline', 'clear']],
+            ['fontname', ['fontname']],
+            ['color', ['color']],
+            ['para', ['ul', 'ol', 'paragraph']],
+            ['table', ['table']],
+            ['insert', ['link', 'picture',]],
+            ['view', ['fullscreen', 'help']],
+        ],
+
+        # Or, explicitly set language/locale for editor
+        'lang': 'ko-KR',
+
+
+        # You can also add custom settings for external plugins
+        # 'print': {
+        #     'stylesheetUrl': '/some_static_folder/printable.css',
+        # },
+        'codemirror': {
+            'mode': 'htmlmixed',
+            'lineNumbers': 'true',
+            # You have to include theme file in 'css' or 'css_for_inplace' before using it.
+            'theme': 'monokai',
+        },
+    },
+
+    # Require users to be authenticated for uploading attachments.
+    'attachment_require_authentication': True,
+
+    # You can completely disable the attachment feature.
+    'disable_attachment': False,
+
+    # Set to `False` to return attachment paths in relative URIs.
+    'attachment_absolute_uri': True,
+
+    # test_func in summernote upload view. (Allow upload images only when user passes the test)
+    # https://docs.djangoproject.com/en/2.2/topics/auth/default/#django.contrib.auth.mixins.UserPassesTestMixin
+
+    # You can add custom css/js for SummernoteWidget.
+}
+# https://github.com/mozilla/bleach / https://github.com/lqez/django-summernote/blob/main/django_summernote/settings.py#L106-L133
+# bleach.clean(tags=ALLOWED_TAGS, attribute=ATTRIBUTES)
+# ALLOWED_TAGS = [
+#     'a', 'div', 'p', 'span', 'img', 'em', 'i', 'li', 'ol', 'ul', 'strong', 'br',
+#     'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
+#     'table', 'tbody', 'thead', 'tr', 'td',
+#     'abbr', 'acronym', 'b', 'blockquote', 'code', 'strike', 'u', 'sup', 'sub',
+# ]
+#
+# STYLES = [
+#     'background-color', 'font-size', 'line-height', 'color', 'font-family'
+# ]
+#
+# ATTRIBUTES = {
+#     '*': ['style', 'align', 'title', ],
+#     'a': ['href', ],
+# }
